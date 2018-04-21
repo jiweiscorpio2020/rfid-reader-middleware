0e427490d784a603ede39804e1d83357d6ef411c
Yago Luiz <yago.luiz@hotmail.com>
2018-04-21 13:06:36 -0300

    Create factory connection

--- src/main/java/com/unb/rfid/reader/repository/AzureRepository.java
+++ src/main/java/com/unb/rfid/reader/repository/AzureRepository.java
@@ -2,6 +2,7 @@ package com.unb.rfid.reader.repository;
 
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.table.*;
+import com.unb.rfid.reader.context.AzureContext;
 import com.unb.rfid.reader.models.AntennaModel;
 import com.unb.rfid.reader.models.LogModel;
 
@@ -9,27 +10,21 @@ import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 
 public class AzureRepository {
-    private static final String CONNECTION_STRING =
-            "DefaultEndpointsProtocol=https;AccountName=unbstoragedev;" +
-                    "AccountKey=fKJppncHugzneTpp7tc7jOdaL4MFvnqmcqIpYdgt64TkKj14jbWUFpw/yAXIPo4ZKQeHzsdE6/WVhj1L4dRlIQ==;" +
-                    "EndpointSuffix=core.windows.net";
-    private static CloudTableClient cloudTableClient;
+
+    private AzureContext azureContext;
 
     public AzureRepository() throws URISyntaxException, InvalidKeyException {
-        if (cloudTableClient == null) {
-            CloudStorageAccount storageAccount = CloudStorageAccount.parse(CONNECTION_STRING);
-            cloudTableClient = storageAccount.createCloudTableClient();
-        }
+        this.azureContext = new AzureContext();
     }
 
     public void insertTableAntenna(AntennaModel antennaModel) throws URISyntaxException, StorageException {
-        CloudTable table = cloudTableClient.getTableReference("Antenna");
+        CloudTable table = azureContext.Antenna();
         TableOperation tableOperation = TableOperation.insert(antennaModel);
         table.execute(tableOperation);
     }
 
     public void insertTableLog(LogModel logModel) throws URISyntaxException, StorageException {
-        CloudTable table = cloudTableClient.getTableReference("Log");
+        CloudTable table = azureContext.Log();
         TableOperation tableOperation = TableOperation.insert(logModel);
         table.execute(tableOperation);
     }
